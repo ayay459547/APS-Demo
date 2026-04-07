b45a33111b0c258fe967ddaba10e6c2cca229915
Caleb <ayay459547@gmail.com>
2026-04-07 17:58:32 +0800

    feat: BrowserRouter add basename

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
